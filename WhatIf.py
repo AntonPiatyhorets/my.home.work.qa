a434d466e3b656ae723140037ae5d7748c970c7e
AntonPiatyhorets <anton.piatyhorets@yahoo.com>
2021-10-06 20:12:49 +0300

    Third Python HW

---
+++ WhatIf.py
@@ -0,0 +1,23 @@
+def calc_function(a, b):
+    if first_number > second_number:
+        print(f'Your first number [{first_number}] bigger than your second number [{second_number}]')
+    elif first_number < second_number:
+        print(f'Your first number [{first_number}] smaller than your second number [{second_number}]')
+    else:
+        print(f'Your first number [{first_number}] equal to your second number [{second_number}]')
+
+
+first_number = int(input('Write first number: '))
+second_number = int(input('Write second number: '))
+
+calc_function(first_number, second_number)
+
+
+
+#Вы задумывались, а что если?..
+ 
+#Необходимо познакомиться с прекрасным IF!
+#Пишем функцию, которая будет принимать два аргумента (а, б).
+#Функция сравнивает А с Б. Если А больше, то функция выводит сообщение: А больше.
+#Если А меньше Б - сообщени, что А меньше
+#Если аргументы равны - сообщение об этом
